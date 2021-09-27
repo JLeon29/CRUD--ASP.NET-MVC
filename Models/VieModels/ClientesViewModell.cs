60f3bff3c9f74b39c5d0b0d02c37592b53a98d86
Juan León <91449845+JLeon29@users.noreply.github.com>
2021-09-27 14:51:46 -0500

    Add files via upload

---
+++ Models/VieModels/ClientesViewModell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LUCKYSAC.Models.VieModels
+{
+    public class ClientesViewModell
+    {
+        public int IdCliente { get; set; }
+        [Required]
+        [StringLength(30)]
+        [Display(Name ="Nombres")]
+        public string Nombres { get; set; }
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Apellidos")]
+        public string  Apellidos { get; set; }
+        [Required]
+        [StringLength(8)]
+        [Display(Name = "DNI")]
+        public string DNI { get; set; }
+        [Required]
+        [StringLength(15)]
+        [Display(Name ="Telefono")]
+        public string Telefono { get; set; }
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        [Display(Name ="Correo Electronico")]
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
